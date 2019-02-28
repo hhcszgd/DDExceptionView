b50ed11e77d5db145d34e3c305e0fd5edb91f5e1
WY <wy@WYdeMac-mini.local>
2019-02-28 09:48:16 +0800

    first init

---
+++ DDExceptionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DDExceptionView
+//
+//  Created by WY on 2019/2/28.
+//  Copyright © 2019 WY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
